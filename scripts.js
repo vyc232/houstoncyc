7f7b11d8fc8c2a437544f36fac8c6564c603515b
Vincent Chow <chow_vincent@utexas.edu>
2017-01-10 12:34:28 -0600

    added current page functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-//TODO: ADD ACTIVE CLASS AND REMOVE IT WHEN HOVERING
+//TODO:REMOVE IT WHEN HOVERING
+
+$(function() {
+	//	Adds current page functionality
+	$("[href]").each(function(){
+		if(this.href == window.location.href){
+			$(this).addClass("active");
+		}
+	});	
+	
+	$('.navbarlist li').on('mouseover click', function(){
+		$('h1').css('color', 'green');			
+	});
+	
+	$('.navbarlist li').on('mouseout', function(){
+		$('h1').css('color', 'blue');
+
+	});
+});
